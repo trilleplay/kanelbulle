3a372f481d33d50fd017bc9e19e7e8db62a6c0b9
JohnyTheCarrot <tuurmartens4@gmail.com>
2019-07-11 02:55:49 -0700

    forgot one

--- bot/utils/get_prefix.py
+++ bot/utils/get_prefix.py
@@ -1,11 +1,12 @@
 from discord.ext.commands import when_mentioned_or
+import config
 
 async def get_prefix(bot, message):
-    database = bot.client["kanelbulle"]
+    database = bot.client[config.database_name]
     servers = database["servers"]
     query = servers.find({"id": message.guild.id}, {"_id": 0}).limit(1)
     if not message.guild or query.count() == 0:
-	       return '>.'
+	       return '<.'
     prefix = ""
     for server in query:
         prefix = server["prefix"]
